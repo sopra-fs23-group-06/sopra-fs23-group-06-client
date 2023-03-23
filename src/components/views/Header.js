56633e6cf132f76409bd0fc22661e120cdcf9347
niko111 <niko111@bluewin.ch>
2023-03-23 11:53:42 +0100

    header change

--- src/components/views/Header.js
+++ src/components/views/Header.js
@@ -13,7 +13,7 @@ import "styles/views/Header.scss";
  */
 const Header = props => (
   <div className="header container" style={{height: props.height}}>
-    <h1 className="header title">SoPra FS23 rocks with React!</h1>
+    <h1 className="header title">SoPra FS23 Group 06</h1>
     <ReactLogo width="60px" height="60px"/>
   </div>
 );
