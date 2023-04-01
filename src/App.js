5723cf8d98a1c44b5898a8e84dd2f2a6bc207a4b
Terebos <95576617+Terebos@users.noreply.github.com>
2023-04-01 21:33:54 +0200

    Update App.js
    removed header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import Header from "components/views/Header";
 import AppRouter from "components/routing/routers/AppRouter";
 
-/**
- * Happy coding!
- * React Template by Lucas Pelloni
- * Overhauled by Kyrill Hux
- */
 const App = () => {
   return (
     <div>
-      <Header height="100"/>
       <AppRouter/>
     </div>
   );
