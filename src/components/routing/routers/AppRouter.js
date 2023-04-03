733d27eea5b97c2ebd29cf5790924636dc3d5298
Terebos <95576617+Terebos@users.noreply.github.com>
2023-04-03 17:46:01 +0200

    edit routing

--- src/components/routing/routers/AppRouter.js
+++ src/components/routing/routers/AppRouter.js
@@ -2,17 +2,11 @@ import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
 import {GameGuard} from "components/routing/routeProtectors/GameGuard";
 import GameRouter from "components/routing/routers/GameRouter";
 import {LoginGuard} from "components/routing/routeProtectors/LoginGuard";
-import Login from "components/views/Login";
+import MainMenu from "components/views/MainMenu";
+import Username from "components/views/Username";
+import LobbyCode from "components/views/LobbyCode";
 
-/**
- * Main router of your application.
- * In the following class, different routes are rendered. In our case, there is a Login Route with matches the path "/login"
- * and another Router that matches the route "/game".
- * The main difference between these two routes is the following:
- * /login renders another component without any sub-route
- * /game renders a Router that contains other sub-routes that render in turn other react components
- * Documentation about routing in React: https://reacttraining.com/react-router/web/guides/quick-start
- */
+
 const AppRouter = () => {
   return (
     <BrowserRouter>
@@ -22,13 +16,28 @@ const AppRouter = () => {
             <GameRouter base="/game"/>
           </GameGuard>
         </Route>
-        <Route exact path="/login">
+        <Route exact path="/main">
           <LoginGuard>
-            <Login/>
+            <MainMenu/>
+          </LoginGuard>
+        </Route>
+        <Route exact path="/join/code">
+          <LoginGuard>
+            <LobbyCode/>
+          </LoginGuard>
+        </Route>
+        <Route exact path="/join/username">
+          <LoginGuard>
+            <Username/>
+          </LoginGuard>
+        </Route>
+        <Route exact path="/host/username">
+          <LoginGuard>
+            <Username/>
           </LoginGuard>
         </Route>
         <Route exact path="/">
-          <Redirect to="/game"/>
+          <Redirect to="/main"/>
         </Route>
       </Switch>
     </BrowserRouter>
