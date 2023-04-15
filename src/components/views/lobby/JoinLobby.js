142a79d70d06da903ce48c5e6d6ad616e6ea5502
Terebos <95576617+Terebos@users.noreply.github.com>
2023-04-15 23:47:28 +0200

    Update JoinLobby.js

--- src/components/views/lobby/JoinLobby.js
+++ src/components/views/lobby/JoinLobby.js
@@ -48,13 +48,13 @@ const JoinLobby = () => {
             user.id = localStorage.getItem("userId");
             user.lobby = localStorage.getItem("lobbyCode");
             await api.put(`/lobbies/${getLobby()}/leaveHandler`, user);
-            returnToMain(false)
+            returnToMain()
         } catch (error) {
             alert(`Something went wrong while leaving the lobby: \n${handleError(error)}`);
         }
     }
 
-function returnToMain(kicked) {
+function returnToMain() {
     localStorage.removeItem("lobbyCode")
     localStorage.removeItem("userId")
     history.push("/")
@@ -69,7 +69,7 @@ function returnToMain(kicked) {
                 setUsers(response.data);
                 const userExists = response.data.some(user => user.id === parseInt(localStorage.getItem("userId")));
                 if (!userExists){
-                    returnToMain(true);
+                    returnToMain();
                 }
             } catch (error) {
                 console.error(`Something went wrong while fetching the users: \n${handleError(error)}`);
@@ -91,7 +91,7 @@ function returnToMain(kicked) {
             source.removeEventListener('message', handleSSE);
             source.close();
         };
-    }, []);
+    }, [returnToMain()]);
 
 
     //need to figure out how to better move buttons to the right
