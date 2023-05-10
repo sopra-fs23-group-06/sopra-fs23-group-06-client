104d537ab527ccd8c918ecfb27ee318377ee8196
bzns <matej.gurica@uzh.ch>
2023-05-10 12:48:47 +0200

    Created timer for player to make a bid. Closes #61.

--- src/components/ui/MakeBid.js
+++ src/components/ui/MakeBid.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'styles/ui/MakeBid.scss';
 import { ButtonPurpleMain } from './ButtonMain';
 
@@ -6,6 +6,24 @@ const MakeBid = ({ roundNumber, onSubmit }) => {
   const [bid, setBid] = useState('');
   const [isValid, setIsValid] = useState(false);
   const maxBid = roundNumber;
+  const [secondsLeft, setSecondsLeft] = useState(35);
+  const [bidSubmitted, setBidSubmitted] = useState(false);
+
+  useEffect(() => {
+    if (secondsLeft === 0 && !bidSubmitted) {
+      onSubmit(0);
+      //clearTimeout(timerId);
+    }
+  }, [secondsLeft, bidSubmitted, onSubmit]);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      if (secondsLeft > 0) {
+        setSecondsLeft(secondsLeft - 1);
+      }
+    }, 1000);
+    return () => clearTimeout(timerId);
+  }, [secondsLeft]);
 
   const handleChange = (event) => {
     const value = parseInt(event.target.value);
@@ -16,33 +34,35 @@ const MakeBid = ({ roundNumber, onSubmit }) => {
   const handleConfirm = (event) => {
     showScream();
     event.preventDefault();
+    setBidSubmitted(true);
     onSubmit(bid);
   };
 
   function showScream() {
     const scream = document.createElement("div");
     scream.classList.add("scream");
-  
+
     const screamContent = document.createElement("div");
     screamContent.classList.add("scream-content");
     screamContent.innerText = "YO-HO-HO!";
-  
+
     scream.appendChild(screamContent);
     document.body.appendChild(scream);
   }
 
   return (
-    <div className="make-bid">
-      <div className="make-bid-header">
-        <h2>Round {roundNumber}</h2>
+      <div className="make-bid">
+        <div className="make-bid-header">
+          <h2>Round {roundNumber}</h2>
+        </div>
+        <div className="make-bid-content">
+          <label htmlFor="bid-input">Make bid:</label>
+          <input type="number" id="bid-input" min="0" max={maxBid} value={bid} onChange={handleChange} />
+          <div className="timer">Time left: {secondsLeft}s</div>
+          <ButtonPurpleMain onClick={handleConfirm} disabled={!isValid}>Confirm</ButtonPurpleMain>
+        </div>
       </div>
-      <div className="make-bid-content">
-        <label htmlFor="bid-input">Make bid:</label>
-        <input type="number" id="bid-input" min="0" max={maxBid} value={bid} onChange={handleChange} />
-        <ButtonPurpleMain onClick={handleConfirm} disabled={!isValid}>Confirm</ButtonPurpleMain>
-      </div>
-    </div>
   );
 };
 
-export default MakeBid
+export default MakeBid;
